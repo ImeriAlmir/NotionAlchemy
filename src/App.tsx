5a6749ec7dba176d965aaa93e0625f0215273c29
Almir Imeri <almir.tu16@gmail.com>
2023-05-08 17:59:14 +0200

    add base landing page component and switch from component to element in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import { QuoteWidget, ClockWidget, PomodoroWidget } from "./shared/components";
-import { CreateWidgetPage, AllWidgetsPage, UserPage, LoadWidgetByIdPage } from "./pages";
+import { CreateWidgetPage, AllWidgetsPage, UserPage, LoadWidgetByIdPage, LandingPage } from "./pages";
 
 const App = () => {
     return (
         <div className="app">
             <Router>
                 <Routes>
-                    <Route path="" Component={AllWidgetsPage}></Route>
-                    <Route path="/user" Component={UserPage}></Route>
+                    <Route path="" element={<LandingPage />}></Route>
+                    <Route path="/all" element={<AllWidgetsPage />}></Route>
+                    <Route path="/user" element={<UserPage />}></Route>
                     <Route path="/clock" element={<ClockWidget widgetTheme="light" />}></Route>
-                    <Route path="/pomodoro" Component={PomodoroWidget}></Route>
-                    <Route path="/quotes" Component={QuoteWidget}></Route>
-                    <Route path="/create" Component={CreateWidgetPage}></Route>
-                    <Route path="/widgets/:widgetId" Component={LoadWidgetByIdPage}></Route>
+                    <Route path="/pomodoro" element={<PomodoroWidget />}></Route>
+                    <Route path="/quotes" element={<QuoteWidget />}></Route>
+                    <Route path="/create" element={<CreateWidgetPage />}></Route>
+                    <Route path="/widgets/:widgetId" element={<LoadWidgetByIdPage />}></Route>
                 </Routes>
             </Router>
-        </div>
+        </div >
     );
 };
 
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ import AllWidgetsPage from "./AllWidgetsPage/AllWidgetsPage";
 import CreateWidgetPage from "./CreateWidgetPage/CreateWidgetPage";
 import UserPage from "./UserPage/UserPage";
 import LoadWidgetByIdPage from "./LoadWidgetByIdPage/LoadWidgetByIdPage";
+import LandingPage from "./LandingPage/LandingPage";
 
-export { AllWidgetsPage, CreateWidgetPage, UserPage, LoadWidgetByIdPage };
+export { AllWidgetsPage, CreateWidgetPage, UserPage, LoadWidgetByIdPage, LandingPage };
