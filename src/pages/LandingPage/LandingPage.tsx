46ca43ddd3b4c1bb800501c782efecc426a34b47
Almir Imeri <almir.tu16@gmail.com>
2023-05-08 21:01:37 +0200

    add first version of landing page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import './LandingPage.scss';
+import { QuoteWidget } from '../../shared/components';
 
 const LandingPage: React.FC = () => {
     return (
-        <div>LandingPage</div>
+        <div>
+            <div className="content-container flex">
+                <div className="left-container flex column center">
+                    <div className="title-container text-center">
+                        <h1 className="title">Notion<br />Alchemy</h1>
+                    </div>
+                    <div className="slogan-container text-center">
+                        <h4 className="slogan">Elevate your Notion experience</h4>
+                    </div>
+
+                </div>
+                <div className="right-container flex column center">
+                    <div className="widget-container flex column center">
+                        <QuoteWidget />
+                        <button className="button">Get Started</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,4 +4,10 @@ import UserPage from "./UserPage/UserPage";
 import LoadWidgetByIdPage from "./LoadWidgetByIdPage/LoadWidgetByIdPage";
 import LandingPage from "./LandingPage/LandingPage";
 
-export { AllWidgetsPage, CreateWidgetPage, UserPage, LoadWidgetByIdPage, LandingPage };
+//export { AllWidgetsPage, CreateWidgetPage, UserPage, LoadWidgetByIdPage, LandingPage };
+
+export { default as AllWidgetsPage } from "./AllWidgetsPage/AllWidgetsPage";
+export { default as CreateWidgetPage } from "./CreateWidgetPage/CreateWidgetPage";
+export { default as UserPage } from "./UserPage/UserPage";
+export { default as LoadWidgetByIdPage } from "./LoadWidgetByIdPage/LoadWidgetByIdPage";
+export { default as LandingPage } from "./LandingPage/LandingPage";
