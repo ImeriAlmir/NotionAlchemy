5ffb70006b341bfe61e4bd7aa30ba3f222aa4cbc
Almir Imeri <almir.tu16@gmail.com>
2023-05-12 15:45:12 +0200

    fixed layout

--- src/pages/CreateWidgetPage/CreateWidgetPage.tsx
+++ src/pages/CreateWidgetPage/CreateWidgetPage.tsx
@@ -15,13 +15,9 @@ const CreateWidgetPage: React.FC = () => {
                 <Card imageUrl={clockImage} imageAltText={altText} widgetName="Clock" widgetDescription="Digital in 24h format" widgetCategory="Clock" />
                 <Card imageUrl={clockImage} imageAltText={altText} widgetName="Pomodoro" widgetDescription="Pomodoro Timer" widgetCategory="Timers" />
                 <Card imageUrl={clockImage} imageAltText={altText} widgetName="Motivation Quotes" widgetDescription="Random Motivation Quotes" widgetCategory="Quotes" />
-            </div>
-            <div className="row flex">
                 <Card imageUrl={clockImage} imageAltText={altText} widgetName="Spotify" widgetDescription="Manage your spotify via Notion" widgetCategory="Music" />
                 <Card imageUrl={clockImage} imageAltText={altText} widgetName="Calendar" widgetDescription="Manage your appointments" widgetCategory="Weather" />
                 <Card imageUrl={clockImage} imageAltText={altText} widgetName="Weather" widgetDescription="Have a look at your weather" widgetCategory="Calendar" />
-            </div>
-            <div className="row flex">
                 <Card imageUrl={clockImage} imageAltText={altText} widgetName="Lorem Ipsum" widgetDescription="Lorem Ipsum" widgetCategory="Clock" />
             </div>
         </div>
